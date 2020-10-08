47212da555490794d87933a59e42a3ee1f86c202
Crystal Norman <cnorm88@gmail.com>
2020-10-08 01:18:07 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,57 +1,29 @@
 require 'pry'
 
-# class Author
+class Author
   
   attr_accessor :name
  
-#   def initialize(name)
-#     @name = name
-#   end
- 
-#   def add_post(post)
-#     post.author = self
-#   end
- 
-#   def posts
-#     Post.all.select {|post| post.author == self}
-#   end
-  
-# def add_post_by_title(title)
-#     post = Post.new(title)
-#     post.author = self
-#   end  
-  
-#   def self.post_count
-#         Post.all.length {|post| post.author == self}
-#   end
-  
-# end
-
-class Author
-
-  attr_accessor :name, :posts
-
-  @@post_count = 0
-  
   def initialize(name)
     @name = name
-    @posts = []
   end
-
+ 
   def add_post(post)
-    @posts << post
     post.author = self
-    @@post_count += 1
   end
-
-  def add_post_by_title(title)
+ 
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+  
+def add_post_by_title(title)
     post = Post.new(title)
-    @posts << post
     post.author = self
-    @@post_count += 1
-  end
-
+  end  
+  
   def self.post_count
-    @@post_count
+        Post.all.length {|post| post.author == self}
   end
+  
 end
+
